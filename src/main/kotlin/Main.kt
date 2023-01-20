6e5904aed7a3df8819b0f8cf0e9aff3249d9c30d
mac <mosktk9577@naver.com>
2023-01-20 16:50:54 +0900

    반복문을 해봤습니다~

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,23 +2,18 @@ import java.util.Scanner
 
 fun main(args: Array<String>) = with(Scanner(System.`in`)){
 
-    val a = nextInt();
-    val b = nextInt();
+    val a = nextInt()
 
-    print((2 * b) - a)
-
-
-
-    fun sum(a: Int, b: Int): Int{
-        return a + b;
+    for(i: Int in 1..a) {
+        println(" @@@   @@@ ")
+        println("@   @ @   @")
+        println("@    @    @")
+        println("@         @")
+        println(" @       @ ")
+        println("  @     @  ")
+        println("   @   @   ")
+        println("    @ @    ")
+        println("     @     ")
     }
-    print(sum(a, b))
 
-
-
-    println("")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
